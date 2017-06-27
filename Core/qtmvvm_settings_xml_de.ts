cc9d589bb50e1285780ecd0f2984e15bbd1235a1
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-27 12:57:37 +0200

    ..

--- Core/qtmvvm_settings_xml_de.ts
+++ Core/qtmvvm_settings_xml_de.ts
@@ -2,26 +2,16 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
-    <name>qtmvvm_settings_xml</name>
+    <name>settings.xml</name>
     <message>
-        <location filename="../../build-SeasonProxer-Desktop_Qt_5_9_0_GCC_64bit-Debug/Core/.qtmvvm_settings_xml_ts_dummy.cpp" line="+6"/>
+        <location filename="../../build-SeasonProxer-Desktop_Qt_5_9_0_GCC_64bit-Debug/Core/.qtmvvm_settings_xml_ts_dummy.cpp" line="+4"/>
         <source>&amp;Automatic Season Check</source>
         <translation>&amp;Automatischer Season-Check</translation>
     </message>
     <message>
-        <location line="-2"/>
-        <source>If checking for too many seasons at once, proxer.me may block your ip. To prevent this, limit the amount of checked animes per day (0 equals no limit).</source>
-        <translation>Wenn zu viele Animes überprüft werden, kann es sein, dass proxer.me die IP blockiert. Gibt das Limit für Checks pro Tag an(0 heißt kein Limit).</translation>
-    </message>
-    <message>
-        <location line="+6"/>
-        <source>&amp;Unmark new seasons</source>
-        <translation>Neue Seasons &amp;demarkieren</translation>
-    </message>
-    <message>
-        <location line="-1"/>
-        <source>Such an action will automatically open the animes relations in your browser.</source>
-        <translation>Eine solche Aktion öffnet Animes automatisch im Browser.</translation>
+        <location line="+8"/>
+        <source>Long/Double click actions</source>
+        <translation>Lang/Doppel-Klick Aktionen</translation>
     </message>
     <message>
         <location line="-2"/>
@@ -29,24 +19,34 @@
         <translation>Animes im &amp;Browser öffnen</translation>
     </message>
     <message>
-        <location line="+4"/>
+        <location line="+1"/>
+        <source>Such an action will automatically open the animes relations in your browser.</source>
+        <translation>Eine solche Aktion öffnet Animes automatisch im Browser.</translation>
+    </message>
+    <message>
+        <location line="-4"/>
+        <source>&amp;Unmark new seasons</source>
+        <translation>Neue Seasons &amp;demarkieren</translation>
+    </message>
+    <message>
+        <location line="+1"/>
         <source>Such an action will remove the &apos;new seasons&apos; mark from the anime, if it had one.</source>
         <translation>Eine solche Aktion entfernt automatisch die &quot;neue Seasons&quot; Information.</translation>
     </message>
     <message>
-        <location line="+1"/>
-        <source>Long/Double click actions</source>
-        <translation>Lang/Doppel-Klick Aktionen</translation>
+        <location line="-2"/>
+        <source>If checking for too many seasons at once, proxer.me may block your ip. To prevent this, limit the amount of checked animes per day (0 equals no limit).</source>
+        <translation>Wenn zu viele Animes überprüft werden, kann es sein, dass proxer.me die IP blockiert. Gibt das Limit für Checks pro Tag an(0 heißt kein Limit).</translation>
     </message>
     <message>
-        <location line="-7"/>
-        <source>Automatically check for new seaons every given interval.</source>
-        <translation>Automatisch nach neuen Seasons suche, nach gegebenen Intervall.</translation>
-    </message>
-    <message>
-        <location line="+3"/>
+        <location line="-1"/>
         <source>Season check &amp;limit</source>
         <translation>Season-Check &amp;Limit</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Automatically check for new seaons every given interval.</source>
+        <translation>Automatisch nach neuen Seasons suche, nach gegebenen Intervall.</translation>
     </message>
 </context>
 </TS>
