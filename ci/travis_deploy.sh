6cea316a5ecb32f6c433475a85234411ad442c48
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-15 14:59:06 +0200

    find apksigner

--- ci/travis_deploy.sh
+++ ci/travis_deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ $PLATFORM == "android_"* ]]; then
 	mkdir -p ./install
-	$HOME/android/sdk/build-tools/*/apksigner sign --ks "$(dirname $0)/anirem.aks" --ks-pass env:AKS_DATA --key-pass env:AKS_DATA --out "./install/anirem-${ANIREM_VER}_${PLATFORM}.apk" "build-$PLATFORM/android-build/build/outputs/apk/android-build-release-unsigned.apk"
+	$(find "$HOME/android" -name apksigner) sign --ks "$(dirname $0)/anirem.aks" --ks-pass env:AKS_DATA --key-pass env:AKS_DATA --out "./install/anirem-${ANIREM_VER}_${PLATFORM}.apk" "build-$PLATFORM/android-build/build/outputs/apk/android-build-release-unsigned.apk"
 else
 	cd install
 	
