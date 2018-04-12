515f3ddeff5b507f426fa134724da880e058b63b
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-04-12 21:01:28 +0200

    automatic version number generation for android

--- ci/droid_build.sh
+++ ci/droid_build.sh
@@ -2,6 +2,17 @@
 set -e
 
 if [[ $PLATFORM == "android_"* ]]; then
+	if [[ "$PLATFORM" == "android_armv7" ]]
+		PCODE=0
+	fi
+	if [[ "$PLATFORM" == "android_x86" ]]
+		PCODE=1
+	fi
+	
+	sed -i "s/android:versionCode=\"\([[:digit:]]*\)\"/android:versionCode=\"\1%{pcode}\"/g" gui/quick/android/AndroidManifest.xml
+	sed -i "s/%{pcode}/$PCODE/g" gui/quick/android/AndroidManifest.xml
+	cat gui/quick/android/AndroidManifest.xml
+
 	mv install build-$PLATFORM/android-build
 	/opt/qt/$QT_VER/$PLATFORM/bin/androiddeployqt --input "build-$PLATFORM/gui/quick/android-libanirem-activity.so-deployment-settings.json" --output "build-$PLATFORM/android-build" --deployment bundled --gradle --no-gdbserver --release
 fi
